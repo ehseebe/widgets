870393c8e2fad55e88fce6dd5b69e12442cf6943
Alyssa Beaton Tanguay <ehseebe@gmail.com>
2020-11-03 15:09:19 -0500

    setting state and jsx styling with semantic-ui

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
-const Dropdown = () => {
+const Dropdown = ({ options }) => {
+
+  const renderedOptions = options.map((option) => {
+    return (
+      <div key={option.value} className="item">
+        {option.label}
+      </div>
+    )
+  })
   return (
-    <div>
-      DropDown
+    <div className="ui form">
+      <div className="field">
+        <label className="label">Select a colour</label>
+        <div className="ui selection dropdown visible active">
+          <i className="dropdown icon"></i>
+          <div className="text">Select colour</div>
+          <div className="menu visible transition">
+          {renderedOptions}
+          </div>
+        </div>
+      </div>
     </div>
   )
 }

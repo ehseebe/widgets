de7a8463180de947be47c420a86053f906b92fd8
Alyssa Beaton Tanguay <ehseebe@gmail.com>
2020-11-03 10:24:33 -0500

    map over items array

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -2,9 +2,22 @@ import React from 'react';
 
 
 const Accordion = ({ items }) => {
+  const renderedItems = items.map((item) => {
+    return(
+      <div key={item.title}>
+        <div className="title active">
+          <i className="dropdown icon"></i>
+          {item.title}
+        </div>
+        <div className="content active">
+          <p>{item.content}</p>
+        </div>
+      </div>
+    )
+  })
 
   return (
-    <div>{items.length}</div>
+    <div>{renderedItems}</div>
   )
 }
 

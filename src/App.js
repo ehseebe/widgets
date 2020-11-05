f685f7f29ee3b5dbd3e2cbb2ec09cf90e1cb3ccf
Alyssa Beaton Tanguay <ehseebe@gmail.com>
2020-11-05 10:44:05 -0500

    testing route component

--- src/App.js
+++ src/App.js
@@ -1,57 +1,68 @@
-import React, {useState} from 'react';
-import Accordion from './components/Accordion';
-import Search from './components/Search';
-import Dropdown from './components/DropDown';
-import Translate from './components/Translate';
+import React, { useState } from "react";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
+import Dropdown from "./components/DropDown";
+import Translate from "./components/Translate";
+import Route from "./components/Route";
+import Header from './components/Header';
 
 const items = [
   {
-    title: 'What is React?',
-    content: 'React is a frontent javascript framework'
+    title: "What is React?",
+    content: "React is a frontent javascript framework",
   },
   {
-    title: 'Why use React?',
-    content: 'Super cool library that everyone looooves'
+    title: "Why use React?",
+    content: "Super cool library that everyone looooves",
   },
   {
-    title: 'How do you use React?',
-    content: 'By creating components!!!'
-  }
-]
+    title: "How do you use React?",
+    content: "By creating components!!!",
+  },
+];
 
 const options = [
   {
-    label: 'it\'s red',
-    value: 'red'
+    label: "it's red",
+    value: "red",
   },
   {
-    label: 'it\'s green',
-    value: 'green',
+    label: "it's green",
+    value: "green",
   },
   {
-    label: 'it\'s blue',
-    value: 'blue'
-  }
-]
+    label: "it's blue",
+    value: "blue",
+  },
+];
 
 const App = () => {
   const [selected, setSelected] = useState(options[0]);
   // const [showDropdown, setShowDropdown] = useState(true);
 
   return (
-   <div> 
-     {/* <Accordion items={items}/>  */}
-     {/* <Search /> */}
-     {/* <button onClick={() => setShowDropdown(!showDropdown)}>Toggle Dropdown</button>
-     {showDropdown ? 
-     <Dropdown 
-     selected={selected} 
-     onSelectedChange={setSelected} 
-     options={options}
-     /> : null} */}
-     <Translate />
-     </div>
-  )
-}
+    <div>
+      <Header />
+      <Route path="/">
+        {/* one jsx inside of another = child, therefore we receive children in the route component */}
+        <Accordion items={items} />
+      </Route>
+      <Route path="list">
+        <Search />
+      </Route>
+      <Route path="dropdown">
+        <Dropdown
+          selected={selected}
+          onSelectedChange={setSelected}
+          options={options}
+          label="Select a color"
+        />
+      </Route>
+      <Route path="translate">
+        <Translate />
+      </Route>
+    </div>
+  );
+};
 
 export default App;

36223c75d375238d0740b3910c810edc380b2fd3
Alyssa Beaton Tanguay <ehseebe@gmail.com>
2020-11-05 10:05:08 -0500

    show output

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,22 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Convert = ({ language, text }) => {
+  const [translated, setTranslated] = useState('');
 
   useEffect(() => {
-    axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
-      params: {
-        q: text,
-        target: language.value,
-        key: ''//invalid key
-      }
-    })
-
-  }, [language, text])
+    const doTranslation = async () => {
+      const { data } = await axios.post(
+        "https://translation.googleapis.com/language/translate/v2",
+        {},
+        {
+          params: {
+            q: text,
+            target: language.value,
+            key: "", //invalid key
+          },
+        }
+      );
+      setTranslated(data.data.translations[0].translatedText);
+    };
+    doTranslation();
+  }, [language, text]);
+  
   return (
-    <div></div>
+  <div>
+    <h1 className="ui header">{translated}</h1>
+  </div>
   )
-}
+};
 
 export default Convert;
